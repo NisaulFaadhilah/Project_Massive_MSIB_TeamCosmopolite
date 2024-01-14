0b592835194f740b81e03ea7aefd462b21185bd0
Nisaul Faadhilah <faadhilahnisa@gmail.com>
2024-01-14 23:38:24 +0700

    Initial commit

---
+++ app/src/test/java/com/nisapplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.nisapplication.app
+
+import com.nisapplication.app.appcomponents.base.BaseActivity
+import com.nisapplication.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
